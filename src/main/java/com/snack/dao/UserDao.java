c8de3b8bababd1f03496d4701c17d8347295e611
unknown <igrshing@gmail.com>
2016-06-17 12:53:18 +0300

    Test commit

--- src/main/java/com/snack/dao/UserDao.java
+++ src/main/java/com/snack/dao/UserDao.java
@@ -4,7 +4,10 @@ import com.snack.model.User;
 
 public interface UserDao {
 	public void create(User user);
+	
 	public User find(int userId);
+	
 	public void delete(int userId);
+	
 	public void update(User user);
 }
